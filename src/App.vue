6441f23df882f1deb6e351a709df949bc74f0c5f
mung <mung@witty.works>
2020-11-27 13:09:13 +0100

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,126 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="container">
+    <div class="row d-flex">
+      <HelloWorld @filtered="setFilter" />
+      <resultShow :filterArray="filter" :items="companies" />
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from "./components/HelloWorld.vue";
+import resultShow from "./components/result";
 
 export default {
-  name: 'App',
   components: {
-    HelloWorld
-  }
-}
+    HelloWorld,
+    resultShow,
+  },
+  data: () => ({
+    filter: null,
+    companies: [
+      {
+        name: "Company One",
+        category: "Finance",
+        start: 1981,
+        end: 2003,
+        location: "Yangon",
+      },
+      {
+        name: "Company Two",
+        category: "Retail",
+        start: 1992,
+        end: 2008,
+        location: "Mandalay",
+      },
+      {
+        name: "Company Three",
+        category: "Auto",
+        start: 1999,
+        end: 2007,
+        location: "Naypyidaw",
+      },
+      {
+        name: "Company Four",
+        category: "Retail",
+        start: 1989,
+        end: 2010,
+        location: "Naypyidaw",
+      },
+      {
+        name: "Company Five",
+        category: "Technology",
+        start: 2009,
+        end: 2014,
+        location: "Mandalay",
+      },
+      {
+        name: "Six",
+        category: "Finance",
+        start: 1987,
+        end: 2010,
+        location: "Nyipyidaw",
+      },
+      {
+        name: "Company Seven",
+        category: "Auto",
+        start: 1986,
+        end: 1996,
+        location: "Yangon",
+      },
+      {
+        name: "Company Eight",
+        category: "Technology",
+        start: 2011,
+        end: 2016,
+        location: "Nyipyidaw",
+      },
+      {
+        name: "Nine",
+        category: "Retail",
+        start: 1981,
+        end: 1989,
+        location: "Mandalay",
+      },
+    ],
+  }),
+  created() {},
+  methods: {
+    setFilter(filter) {
+      this.filter = filter;
+    },
+  },
+  computed: {
+    filteredList() {
+      switch (this.filter) {
+        case "year":
+          return this.companies.sort((a, b) => a.start - b.start);
+          break;
+        case "Naypyidaw":
+          return this.companies.filter((loc) => loc.location === "Naypyidaw");
+          break;
+        case "between":
+          return this.companies.filter((loc) => loc.start > 1992);
+          break;
+        default:
+          return this.companies;
+          break;
+      }
+
+      //   if(this.inputText) {
+      //  return   this.companies = this.companies.filter(d=>{
+      //      let  allOf = [d.name.toLowerCase(),d.category.toLowerCase(),d.location.toLowerCase()]
+      //    //   return allOf.toString().indexOf(this.inputText.toLowerCase())>-1
+      // return allOf.includes(this.inputText.toLowerCase())
+
+      //       });
+
+      // }
+
+      return this.companies;
+    },
+  },
+};
 </script>
 
 <style>
---
+++ src/components/result.vue
@@ -0,0 +1,85 @@
+<template>
+     <div class="col-6">
+       <input class="form-control m-3" type="text" v-model="inputText" id="inputTest">
+     
+      <table class="table">
+  <thead>
+    <tr>
+      <th scope="col">#</th>
+      <th scope="col">Name</th>
+      <th scope="col">Category</th>
+      <th scope="col">Start</th>
+      <th scope="col">End</th>
+      <th scope="col">Location</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr v-for="(item,i) in items" :key="i">
+      <th scope="row">{{i+1}}</th>
+      <td>{{item.name}}</td>
+      <td>{{item.category}}</td>
+      <td>{{item.start}}</td>
+      <td>{{item.end}}</td>
+        <td>{{item.location}}</td>
+    </tr>
+     
+     </tbody>
+      </table>
+     </div>
+</template>
+
+<script>
+export default {
+    name: 'result',
+   props:{
+    filterArray:{String, default: null},
+       items:{
+           type: Array
+       }
+   },
+    data:()=>({
+     inputText: '',
+   }),
+ 
+   created(){
+    
+  },
+  methods:{
+  },
+
+   computed: {
+//       filteredList() {
+//         let items = JSON.parse(JSON.stringify(this.items));
+
+//         switch (this.filterArray) {
+//           case 'year':
+//             items = items.sort((a,b) => a.start-b.start);
+//             break;
+//          case 'Naypyidaw':
+//           items =  this.items.filter(loc => loc.location === 'Naypyidaw');
+//             break;
+//           case 'between':
+//             items =  this.items.filter(loc=> loc.start > 1992);
+//             break;
+//           default:
+//             items =  this.items;
+//             break;
+//           }
+
+//         if(this.inputText) {
+//        return   items = items.filter(d=>{
+//            let  allOf = [d.name.toLowerCase(),d.category.toLowerCase(),d.location.toLowerCase()]
+//          //   return allOf.toString().indexOf(this.inputText.toLowerCase())>-1
+//       return allOf.includes(this.inputText.toLowerCase())
+  
+//             });
+        
+//         }
+
+// return items;
+//     }
+  },
+
+  }
+
+</script>
